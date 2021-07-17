fb0a5393b6ac737350b96614b091e3ba16f67795
pjoon357 <pjoon357@naver.com>
2021-07-17 17:46:07 +0900

    useEffect 2

---
+++ useScroll/useScroll.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const useScroll = () => {
+    const [state, setState] = useState({ x: 0, y: 0 });
+    const onScroll = () => {
+        setState({ x: window.scrollX, y: window.scrollY });
+    };
+    useEffect(() => {
+        window.addEventListener("scroll", onScroll);
+    }, []);
+    return state;
+};
